56a0092305f09ed76ed60b06b96c85d1d59ca42e
luoshujie <1427339105@qq.com>
2020-03-29 22:42:11 +0800

    添加英雄详情界面

---
+++ Assets/Script/Window/HeroDescWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script.Window
+{
+    public class HeroDescWindow : MonoBehaviour
+    {
+        public Text levelText;
+        public Text nameText;
+        public Image person;
+        public Text minNameText;
+        public Text occupationText;
+        public List<GameObject> starList;
+        public Slider lifeSlider;
+        public Slider defenseSlider;
+        public Slider attackSlider;
+    }
+}
