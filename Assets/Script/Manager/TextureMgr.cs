da63d9bda0dddbce4fcda02d89ff283f1276d7a4
luoshujie <1427339105@qq.com>
2020-04-19 12:40:50 +0800

    添加图片管理

---
+++ Assets/Script/Manager/TextureMgr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.Manager
+{
+    public class TextureMgr : MonoBehaviour
+    {
+        public static TextureMgr instance;
+        private string path = "Sprite/";
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public Sprite GetSprite(int id)
+        {
+            return Resources.Load<Sprite>(path + id);
+        }
+    }
+}
